d4b1f6a971076cb114d51696d95469da67f5c1b0
Nahin-Yassin <nahinyasin32@gmail.com>
2023-07-03 11:24:03 +0600

    countdown

---
+++ main.js
@@ -0,0 +1,33 @@
+const daysEl = document.getElementById('days')
+const hoursEL = document.getElementById('hours')
+const minsEl = document.getElementById('min')
+const secEL = document.getElementById('sec')
+
+const getToDate = ("1 Jan 2024")
+
+function countdown() {
+    const newYearDate = new Date(getToDate);
+    const currentDate = new Date();
+
+    // copy paste data not memorizable 
+
+    const Allsec = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(Allsec / 3600 / 24);
+    const hours = Math.floor(Allsec / 3600) % 24;
+    const min = Math.floor(Allsec / 60) % 60;
+    const sec = Math.floor(Allsec) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEL.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(min);
+    secEL.innerHTML = formatTime(sec);
+
+}
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown()
+
+setInterval(countdown, 1000)
